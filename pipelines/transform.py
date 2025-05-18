fd4af03751676a31a0596053ac1a56a722dee854
duwei-19800501 <duwei20200101@hotmail.com>
2025-05-18 15:20:23 +0800

    add great expectation

--- pipelines/transform.py
+++ pipelines/transform.py
@@ -1,6 +1,25 @@
 import pandas as pd
-def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+import great_expectations as gx
+from typing import Optional
+
+def validate_data(df: pd.DataFrame) -> bool:
+    context = gx.get_context()
+    result = context.run_checkpoint(
+        checkpoint_name="sales_checkpoint",
+        batch_request={
+            "runtime_parameters": {"batch_data": df},
+            "data_asset_name": "post_transform_data",
+            "batch_identifiers": {"default_identifier": "default"}
+        }
+    )
+    return result["success"]
+
+def transform_data(df: pd.DataFrame) -> Optional[pd.DataFrame]:
     df = df.dropna()
     df['total_price'] = df['quantity'] * df['unit_price']
     df['order_date'] = pd.to_datetime(df['order_date'])
+    
+    if not validate_data(df):
+        print("❌ 数据验证失败！详情见 gx/uncommitted/data_docs/")
+        return None
     return df
